b53a7c21fde8365ecef2eb62fca9ba06285fed3e
Kirill Stepanov <k.v.stepanov@icloud.com>
2024-07-25 16:51:40 +0300

    Add type

--- src/app/components/UsersCountRating.tsx
+++ src/app/components/UsersCountRating.tsx
@@ -2,7 +2,7 @@ import { ColoredItem } from "../../styled/ColoredItem";
 
 // TODO: Реализовать отображение пяти пользователей с максимальным счетчиком count в виде строки
 export const UsersCountRating = () => {
-  const rating = "TODO";
+  const rating: string = "";
   return (
     <div>
       <h2>Топ 5 пользователей с максимальным значением count</h2>
