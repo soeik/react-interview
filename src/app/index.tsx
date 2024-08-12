5683270b36122c7fea00c277d6e374d3506f913e
Kirill Stepanov <k.v.stepanov@icloud.com>
2024-08-12 13:49:33 +0300

    Cleanup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,9 @@
 import { Posts } from "./components/Posts";
-import { NameInput } from "./components/NameInput";
 import { UsersCountRating } from "./components/PostsRating";
 
 export const App = () => {
   return (
     <>
-      <NameInput />
       <UsersCountRating />
       <Posts />
     </>
